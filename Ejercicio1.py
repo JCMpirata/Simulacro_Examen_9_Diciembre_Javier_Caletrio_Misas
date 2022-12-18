3229d78de058de352cdbf9c9e7fe9e1c651af432
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 17:57:58 +0100

    mas minion game

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,3 +3,10 @@ def minion_game(string):
     kevin_score = 0
     stuart_score = 0
     substrings = {}
+
+    for i in range(len(string)):
+        if string[i] in vowels:
+            for j in range(i + 1, len(string) + 1):
+                substrings[string[i:j]] = substrings.get(string[i:j], 0) + 1
+            kevin_score = sum(substrings.values())
+            print(substrings)
