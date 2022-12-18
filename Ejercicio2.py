bf1892164a98dd766a559d1eaeec72ff1ceb8ae2
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 18:13:57 +0100

    mas hacker chess

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -9,3 +9,15 @@ def movimiento(jugador, tabla):
             signo = -1
         posiciono = tabla[c].index(jugador)
         posicion = tabla[c].index(jugador)
+
+    for i in range(signo, f+signo, signo):
+            if f < 0:
+                posicion = posicion -1
+                mover = False
+            elif f > 0:
+                posicion = posicion +1
+                mover = False
+            else:
+                pass
+
+    
