64050191f41b653819a6aedc7cbbcc83743249fb
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-12-18 18:11:04 +0100

    hacker chess empezar

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+def movimiento(jugador, tabla):
+    mover = True
+    while mover:    
+        c = int(input("Elige una columna (Introduce un número entero): ", 0, len(tabla)-1))
+        f = int(input("Elige la posicion de la torre (Introduce un número entero negativo para arriba y otro numero entero positivo hacia abajo): ", -(len(tabla)), len(tabla)))
+        if f >=0:
+            signo = 1
+        else:
+            signo = -1
+        posiciono = tabla[c].index(jugador)
+        posicion = tabla[c].index(jugador)
